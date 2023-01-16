8e89533b382875de542f3eb834288238c1997537
rajeshpatil88 <100502337+rajeshpatil88@users.noreply.github.com>
2023-01-16 15:50:30 +0530

    Update NewTextDocument.py

--- NewTextDocument.py
+++ NewTextDocument.py
@@ -4,9 +4,9 @@ list1 = [2,4,6,8,10]
 
 square = [i**2 for i in list1]
 
-print('Square of the numbers are:',square] 
+print('Square of the numbers are:',square)
 
 cube = [i**3 for i in list1]
 
-print('Cube of the numbers are :',cube]
+print('Cube of the numbers are :',cube)
 
