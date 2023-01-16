80cf81dc4fe2e5bed8c8e5f97e3405f93496edbf
rajeshpatil88 <100502337+rajeshpatil88@users.noreply.github.com>
2023-01-16 15:49:36 +0530

    Update NewTextDocument.py

--- NewTextDocument.py
+++ NewTextDocument.py
@@ -2,11 +2,11 @@
 
 list1 = [2,4,6,8,10]
 
-square = [X**2 for i in list1]
+square = [i**2 for i in list1]
 
 print('Square of the numbers are:',square] 
 
-cube = [X**3 for i in list1]
+cube = [i**3 for i in list1]
 
 print('Cube of the numbers are :',cube]
 
